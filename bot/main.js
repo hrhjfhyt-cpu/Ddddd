c34697057b3ccb9cfaee3c9b3fc0709ea73dbc0b
hrhjfhyt-cpu <hrhjfhyt@gmail.com>
2026-08-24 19:11:53 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const ADMINS = new Set(['61590764837326',]);
+const ADMINS = new Set(['61593560139476',]);
 const commands = new Map();
 
 // عداد الرسائل لكل جروب — كل 568 رسالة يتفاعل البوت
